4707ceffa237cead8459fe709c127513b14103b3
Super Dupont <dupontdenis@users.noreply.github.com>
2022-10-29 09:24:50 +0200

    limit the model to books and authors

--- populate.js
+++ populate.js
@@ -1,11 +1,10 @@
 #! /usr/bin/env node
 
-console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
+console.log('This script populates some test books, authors to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
 
 // Get arguments passed on command line
 var userArgs = process.argv.slice(2);
 
-var async = require('async')
 var Book = require('./models/book')
 var Author = require('./models/author')
 
